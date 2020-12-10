79167c6ead0ebac02c859653133e7d1efb19a3c6
Gajendra Kumar <gajendrakumar@Gajendras-MacBook-Pro.local>
2020-12-10 15:50:54 +0530

    first commit

---
+++ src/main/java/com/self/vertx/HelloWorld.java
@@ -0,0 +1,32 @@
+package com.self.vertx;
+
+
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Router;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloWorld
+{
+    public static void main( String[] args )
+    {
+        Vertx vertx = Vertx.vertx();
+        HttpServer httpServer = vertx.createHttpServer();
+        Router router = Router.router(vertx);
+        router
+                .route()
+                .handler(routingContext -> {
+                    HttpServerResponse response = routingContext.response();
+                    response.putHeader("content-type", "text/plain");
+                    response.end("Hello World in vertx");
+                });
+        httpServer
+                .requestHandler(router :: accept)
+                .listen(8091);
+
+    }
+}
---
+++ src/test/java/com/self/vertx/HelloWorldTest.java
@@ -0,0 +1,20 @@
+package com.self.vertx;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class HelloWorldTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
